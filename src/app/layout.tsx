dcad460ab574b1c17d2824c84f090254841c3e43
vandhemer <vandhemer@gmail.com>
2024-09-09 12:37:00 +0200

    Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./styles/globals.css";
 import Page from "./page";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Conforama",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body className={inter.className}>
+        <body>
             <Page />
         </body>
     </html>
