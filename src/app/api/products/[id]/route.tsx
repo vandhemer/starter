c7f029cdca24caf2ed62de60d5ba2e9a1c11cb8b
vandhemer <vandhemer@gmail.com>
2024-09-11 17:39:19 +0200

    Update API

--- src/app/api/products/[id]/route.tsx
+++ src/app/api/products/[id]/route.tsx
@@ -1,10 +1,8 @@
-export async function GET(
-    request: Request,
-    { params }: { params: { id: string } }
-  ) {
+export async function GET( request: Request, { params }: { params: { id: string } } ) {
   
   const res = await fetch('https://cloud-api.conforama.fr/occ/v2/conforama/products/UEFSSVMgTkFUSU9O/fullbasicproducts?productsCodes=' + params.id, {
     headers: {
+      accept: 'application/json',
       'Content-Type': 'application/json',
     },
   })
