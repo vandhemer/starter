059ef87de24104362197446e06976b59c8f7d17e
vandhemer <vandhemer@gmail.com>
2024-09-20 18:22:39 +0200

    Fix fetch

--- src/app/services/product/getProductService.tsx
+++ src/app/services/product/getProductService.tsx
@@ -2,13 +2,19 @@
 import { Product } from '@/models/product/product';
 
 export const fetchProductById = async (id: string): Promise<Product> => {
+  
+  const headers: HeadersInit = {
+    accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  if (process.env.GRAVITEE_API_KEY) {
+    headers['X-Gravitee-Api-Key'] = process.env.GRAVITEE_API_KEY;
+  }
+
   const res = await fetch(process.env.PRODUCT_API_URL + '/products/v1/UEFSSVMgTkFUSU9O/fullproduct?productsCodes=' + id, {
-    headers: {
-      accept: 'application/json',
-      'Content-Type': 'application/json',
-      'X-Gravitee-Api-Key': process.env.GRAVITEE_API_KEY,
-    },
-  })
+    headers,
+  });
   
   return await res.json();
 };
